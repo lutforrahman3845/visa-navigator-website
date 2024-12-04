3f93e1f3053d4708d48afebb1b33882e58ae7c46
Lutfor Rahman <lutforr3845@gmail.com>
2024-12-05 01:52:28 +0600

    create footer

--- src/Pages/Loading.jsx
+++ src/Pages/Loading.jsx
@@ -1,9 +1,8 @@
-import Lottie from "lottie-react";
-import loader from "../assets//loader.json";
+
 const Loading = () => {
     return (
         <div className="flex justify-center min-h-screen dark:bg-secondary">
-            <Lottie className="w-36" animationData={loader} loop={true} />
+           <span className="loading loading-ring loading-lg"></span>
         </div>
     );
 };
--- src/Pages/Root.jsx
+++ src/Pages/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 const Root = () => {
     return (
         <div className="dark:bg-white">
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
             
         </div>
     );
