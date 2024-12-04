3e355861248b2fa7a4e8cf614187603e6fcdfdfa
Lutfor Rahman <lutforr3845@gmail.com>
2024-12-04 15:20:25 +0600

    change login and registration context api

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,12 +1,11 @@
 import { createContext, useEffect, useState } from "react";
 import auth from "../Firebase/firebase.config";
-import { createUserWithEmailAndPassword, onAuthStateChanged, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged,  signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 
 export const AuthContext = createContext()
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const[loading , setLoading] = useState(true);
-    const [email, setEmail] = useState("")
  console.log(user)
     const createNewUser = (email, password) =>{
         setLoading(true)
@@ -25,14 +24,11 @@ const AuthProvider = ({children}) => {
         return signInWithPopup (auth, googleProvider)
     }
     const profileUpDate = (upDatedProfile) =>{
+        setLoading(true)
         return updateProfile(auth.currentUser, upDatedProfile)
     }
     const logOut = ()=>{
-        setLoading(true)
         return signOut(auth)
-    }
-    const resetPassword = (email) =>{
-        return sendPasswordResetEmail(auth, email)
     }
 
     useEffect(()=>{
@@ -56,9 +52,6 @@ const AuthProvider = ({children}) => {
         logOut,
         loading,
         profileUpDate,
-        email,
-        setEmail,
-        resetPassword
     }
 
    
